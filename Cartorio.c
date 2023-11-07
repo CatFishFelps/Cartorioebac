4530ad7d4524381ae4769c2a0eaaf15fb8e45504
Felipe Sozo <123550081+CatFishFelps@users.noreply.github.com>
2023-11-07 18:53:55 -0300

    Nova opção para selecionar.
    Opção 4 para sair do sistema agora foi adicionada.

--- Cartorio.c
+++ Cartorio.c
@@ -135,7 +135,8 @@ int main ()
      	printf("Escolha a opção desejada do menu:\n\n");
     	printf("\t1 - Registrar nomes\n");
     	printf("\t2 - Consultar nomes\n");
-    	printf("\t3 - Deletar nomes\n\n");
+    	printf("\t3 - Deletar nomes\n");
+    	printf("\t4 - Sair do Sistema\n\n");
     	printf("Opção: ");//fim do Menu
 	
 	    scanf("%d", &opcao);//armazenando a escolha do usuário. "%d" é o comando para armazenar o valor na variavel.
@@ -157,6 +158,11 @@ int main ()
 	    	deletar();
     		break;
     		
+    		case 4:
+    	    printf("Obrigado pro útilizar o Sistema!\n");
+    	    return 0;
+    	    break;
+    	    
     		default:
     		printf("Essa opção não está disponível!\n");
     		system("pause");
